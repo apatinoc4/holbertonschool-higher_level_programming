340dd332ea56b0d628a71e38f31859ee5c20eb50
apatinoc4 <apatinoc4@gmail.com>
2019-09-10 19:55:59 +0000

    fixed identation and greater than sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
-if number > 0:
+if number < 0:
     lastdigit = lastdigit * -1
 if lastdigit > 5:
     print('Last digit of', number, "is", lastdigit, 'and is greater than 5')
 elif lastdigit < 6 and lastdigit != 0:
-    print('Last digit of', number, "is", lastdigit, 'and is less than 6 and not 0')
+    print('Last digit of', number, "is", lastdigit,
+          'and is less than 6 and not 0')
 else:
     print('Last digit of', number, "is", lastdigit, 'and is 0')
