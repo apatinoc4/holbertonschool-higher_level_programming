77ce20bccfc5767e96896a9b1eefbbcdb133fd00
apatinoc4 <apatinoc4@gmail.com>
2019-09-17 21:07:50 +0000

    replaced for for else

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
+    length = len(sentence)
+    if length == 0:
+        position = None
     else:
-        return (len(sentence), sentence[0])
+        position = sentence[0]
+        return (length, position)
