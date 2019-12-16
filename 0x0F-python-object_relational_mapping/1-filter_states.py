8ea5095ca97e8bb03f4cd24c81995c7d770ff65a
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-15 20:33:54 -0500

    fixed pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 lists all states with a name starting with N (upper N)
 """
 from sys import argv
 import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name LIKE BINARY
                 'N%' ORDER BY states.id ASC""")
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 script that takes in arguments and displays all values in the states
 """
 
