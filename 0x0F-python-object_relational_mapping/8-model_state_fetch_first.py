1829c95fb8cdd87074b5be1dd6d2f9ad222b5f45
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-15 22:00:55 -0500

    state baseclass  7 8 and 9

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+prints the first State object from the database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                        (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+
+    session = Session(eng)
+    result = session.query(State).order_by(State.id).first()
+    if not result:
+        print("Nothing")
+    else:
+        print("{}: {}".format(result.id, result.name))
+    session.close()
