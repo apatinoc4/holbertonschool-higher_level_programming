86b0f93f7b66adf26bdb0d521330e51b2c6ef503
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-15 22:18:59 -0500

    fixed 11 n 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Add Louisiana to the database
+changes the name of a State object
 """
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                         (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    state = State(name="Louisiana")
-    session.add(state)
+    Base.metadata.create_all(eng)
+    session = Session(eng)
+    change = session.query(State).get(2)
+    change.name = 'New Mexico'
     session.commit()
-    print(state.id)
     session.close()
