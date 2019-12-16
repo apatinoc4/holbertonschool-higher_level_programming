69f41b317426c7d422530b3c1886775364371da7
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-15 20:27:08 -0500

    fixed 2 and 3-5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 
-lists all states with a name starting with N (upper N)
+lists all cities from the database hbtn_0e_4_usa
 """
 from sys import argv
 import MySQLdb
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY
-                'N%' ORDER BY states.id ASC""")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                JOIN states ON cities.state_id=states.id
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 
-lists all states with a name starting with N (upper N)
+akes in the name of a state as an argument and lists all cities
 """
 from sys import argv
 import MySQLdb
@@ -8,11 +8,13 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY
-                'N%' ORDER BY states.id ASC""")
+    cur.execute("""SELECT cities.name FROM cities JOIN states
+            ON cities.state_id=states.id WHERE states.name
+            LIKE BINARY %s ORDER BY cities.id ASC""", (argv[4],))
     rows = cur.fetchall()
+    result = []
     for row in rows:
-        print(row)
+        result.append(row[0])
+    print(", ".join(result))
     cur.close()
     db.close()
-    
