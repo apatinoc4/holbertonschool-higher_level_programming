5cd7da9235b98dabccc6715967d09c64374500fc
root <root@DESKTOP-GQSD5ER.localdomain>
2019-12-15 22:30:23 -0500

    12 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Add Louisiana to the database
+deletes all State objects with a name containing the letter a
 """
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                         (argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
     session = Session(eng)
-    state = State(name="Louisiana")
-    session.add(state)
+    session.query(State).filter(State.name.like('%a%')).delete(
+            synchronize_session='fetch')
     session.commit()
-    print(state.id)
     session.close()
