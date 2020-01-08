88c098a11806b62a15d7c9b0c1e5de4bdc046724
root <root@DESKTOP-GQSD5ER.localdomain>
2020-01-07 21:46:12 -0500

    0-body_size.sh

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Write a function that finds a peak in a list of unsorted integers
+
+
+def find_peak(list_of_integers):
+    """ find_peak main function """
+    if not list_of_integers:
+        return None
+    return finder(list_of_integers, len(list_of_integers),
+                       0, len(list_of_integers) - 1)
+
+
+def finder(array, length, low, high):
+    """ find peak helper function """
+    mid = low + (high - low) // 2
+    if ((mid == 0 or array[mid] >= array[mid - 1]) and
+            (mid == length - 1 or array[mid] >= array[mid + 1])):
+        return array[mid]
+    elif mid > 0 and array[mid - 1] > array[mid]:
+        return finder(array, length, low, mid - 1)
+    else:
+        return finder(array, length, mid + 1, high)
