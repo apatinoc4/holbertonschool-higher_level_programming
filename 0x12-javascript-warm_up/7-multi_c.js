d6e452d233bb095010263d3fad22babdd3062e8a
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 14:53:16 -0500

    fixed identation

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const n = process.argv;
 if (isNaN(n[2])) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < parseInt(n[2]); i++) {
+  for (let i = 0; i < parseInt(n[2]); i++) {
     console.log('C is fun');
   }
 }
