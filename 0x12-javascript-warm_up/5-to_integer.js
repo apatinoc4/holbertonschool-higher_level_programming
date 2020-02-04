13fd6ec9a04ae39deb181265492361b1860ce56a
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 12:50:17 -0500

    remmoved myvar

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const myvar = process.argv[2];
-if (isNaN(parseInt(myvar))) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(myvar));
+  console.log(parseInt(process.argv[2]));
 }
