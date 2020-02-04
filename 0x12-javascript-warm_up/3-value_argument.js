22f40536e06c55a669db3903b75ae69aa45bd4bf
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 12:31:27 -0500

    remmoved myvar

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const myvar = process.argv[2];
-if (myvar == undefined) {
+if (process.argv[2] == undefined) {
   console.log('No argument');
 } else {
-  console.log(myvar);
+  console.log(process.argv[2]);
 }
