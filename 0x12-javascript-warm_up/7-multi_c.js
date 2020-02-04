083d58c18bc3ce7b363a47becb666efa4367ef44
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 14:43:49 -0500

    replaced while with for

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 const n = process.argv;
-let i = 0;
 
 if (isNaN(n[2])) {
   console.log('Missing number of occurrences');
 } else {
-  while (i < parseInt(n[2])) {
+    for (let i = 0; i < parseInt(n[2]); i++) {
     console.log('C is cool');
-    i++;
   }
 }
