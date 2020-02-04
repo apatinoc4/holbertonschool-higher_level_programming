f3ce95bef9f8c8fb256b1f1e7789003313915bfa
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 14:33:34 -0500

    remmoved message and typedi it directly

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
 const n = parseInt(process.argv[2]);
-const message = 'C is cool';
 let i = 0;
 
 if (isNaN(n)) {
   console.log('Missing number of occurrences');
 } else {
   while (i < n) {
-    console.log(message);
+    console.log('C is cool');
     i++;
   }
 }
