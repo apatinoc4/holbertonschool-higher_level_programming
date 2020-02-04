d9d7d505214f614d79270ace2155b9a6b5ab27c1
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 17:23:26 -0500

    semistandard identation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,13 +3,13 @@
 const n1 = parseInt(process.argv[2]);
 
 function factorial (a) {
-    if (a === 0 || a === 1) {
-      return 1;
-    }
-    return factorial(a - 1) * a;
+  if (a === 0 || a === 1) {
+    return 1;
   }
-  if (isNaN(n1)) {
-    console.log(1);
-  } else {
-    console.log(factorial(n1));
-  }
+  return factorial(a - 1) * a;
+}
+if (isNaN(n1)) {
+  console.log(1);
+} else {
+  console.log(factorial(n1));
+}
