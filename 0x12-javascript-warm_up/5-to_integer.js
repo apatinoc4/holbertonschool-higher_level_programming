49d9d7420f91b05503ed774a56e6829e9445678a
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 14:18:57 -0500

    aditional fixes to 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const myvar = parseInt(process.argv[2]);
-if (Number.isNaN(myvar)) {
+
+const n = parseInt(process.argv[2]);
+if (isNaN(n)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + myvar);
+  console.log('My number: ' + n);
 }
