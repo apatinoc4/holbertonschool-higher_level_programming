423bf4fece5b8323501f5020d0e358743a796277
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 14:50:53 -0500

    replaced C is cool to fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ if (isNaN(n[2])) {
   console.log('Missing number of occurrences');
 } else {
     for (let i = 0; i < parseInt(n[2]); i++) {
-    console.log('C is cool');
+    console.log('C is fun');
   }
 }
