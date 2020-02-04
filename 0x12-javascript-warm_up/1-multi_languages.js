04b4b34146208e066f939ebb0f0fa4b5452f7383
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 09:57:27 -0500

    0-1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myvar = 'C is fun';
+const myvar2 = 'Python is cool';
+const myvar3 = 'Javascript is amazing';
+console.log(myvar);
+console.log(myvar2);
+console.log(myvar3);
