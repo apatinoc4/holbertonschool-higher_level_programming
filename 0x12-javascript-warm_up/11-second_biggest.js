65d3804563c8c4f58df32d1d049b63a0c39c1611
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 20:37:15 -0500

    added undefined case and fixed semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
 const unique = (value, index, self) => {
-    return self.indexOf(value) === index
-  }
+  return self.indexOf(value) === index;
+};
 
 const n = process.argv;
 const n2 = n.slice(2);
 const n3 = n2.map(Number);
 const n4 = n3.filter(unique);
 const n5 = n4.sort();
-const n6 = n5[n5.length -2];
-
+const n6 = n5[n5.length - 2];
 
 if (n.length < 3) {
-    console.log(0);
-  } else if (n.length === 3){
-    console.log(0);
-  }
-  else {
+  console.log(0);
+} else if (n.length === 3) {
+  console.log(0);
+} else if (n[2] === undefined) {
+  console.log(0);
+} else {
   console.log(n6);
-  }
+}
