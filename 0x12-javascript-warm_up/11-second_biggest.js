4fd32e7411a0ca2fdba419d79861fb3db4eb8487
apatinoc4 <apatinoc4@gmail.com>
2020-02-04 21:27:32 -0500

    semistandard and booooooooomgit add 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,7 @@ const n = process.argv;
 const n2 = n.slice(2);
 const n3 = n2.map(Number);
 const n4 = n3.filter(unique);
-const n5 = n4.sort(function(a,b) { return a - b; });
+const n5 = n4.sort(function (a, b) { return a - b; });
 const n6 = n5[n5.length - 2];
 
 if (n.length < 3) {
