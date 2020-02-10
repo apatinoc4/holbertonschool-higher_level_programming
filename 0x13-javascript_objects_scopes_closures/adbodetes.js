4a9a91d7cbe0054fc570e847e7d55e6f72f93bab
apatinoc4 <apatinoc4@gmail.com>
2020-02-10 18:33:53 -0500

    4 5 and 6

--- 0x13-javascript_objects_scopes_closures/adbodetes.js
+++ 0x13-javascript_objects_scopes_closures/adbodetes.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor(w, h) {
-        this.widht = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.widht = w;
+    this.height = h;
+  }
 };
