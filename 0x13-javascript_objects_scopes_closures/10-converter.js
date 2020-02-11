f11a909f4ef15678453f3cdebf5e9a7b8bd62a0e
apatinoc4 <apatinoc4@gmail.com>
2020-02-10 22:21:38 -0500

    semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    return (num) => {
-      return num.toString(base);
-    };
+  return (num) => {
+    return num.toString(base);
   };
+};
