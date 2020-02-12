43c434841ba2833043d62ff3a8ad725bdf3eb733
apatinoc4 <apatinoc4@gmail.com>
2020-02-11 20:29:47 -0500

    fixed 0 and permissions

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-  if (err) { console.log(err); } else { console.log(contents); }
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
